02008b0ce5e5c2b80715c07fbd7ff5dd063d7b55
JuanPabloParra <juan_pab.parra@uao.edu.co>
2023-02-14 23:00:04 -0500

    fetch api

--- my-app/src/GifGrid.jsx
+++ my-app/src/GifGrid.jsx
@@ -1,9 +1,21 @@
 import { useEffect } from "react"
 
+const getGifs = async( category) =>{
+    const url = `https://api.giphy.com/v1/gifs/search?q=${(category) }&limit=10&api_key=dc6zaTOxFJmzC`
+    const response = await fetch(url)
+    const { data } = await response.json()
+    const gifs  = data.map ( img => {
+        return{
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    })
+    return gifs
+
+}
 export const GifGrid =({category}) => {
-    useEffect(() => {
-        console.log( category );
-    }, [])
+    getGifs ( category )
     
 
     return(
