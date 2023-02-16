bd6033351a83ff6c5548422a1bdc0786fd573a8b
Juan Pavón <98887772+JacobS1970@users.noreply.github.com>
2023-02-16 18:32:00 -0500

    challenge

--- my-app/src/GifGrid.jsx
+++ my-app/src/GifGrid.jsx
@@ -1,30 +1,20 @@
-
-
-const getGifs = async ( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=dc6zaTOxFJmzC`;
-    const resp = await fetch (url )
-    const { data } = await resp.json()
-    const gifs = data.map( img => {
-        return {
-            id: img.id,
-            title: img.title,
-            url: img.images.downsized_medium.url
-        }
-
-    })
-    return gifs
-
-}
+import { useEffect } from "react";
+import { useFetchGifs } from "./useFetchGifs";
+import { GifItem } from './GifItem';
 
 export const GifGrid = ({category}) => {
-    getGifs(category)
-    return (
+    const {images, isoading}= useFetchGifs(category)
+    return(
         <>
-        <h3> {category}</h3>
-        <p> Hello World </p>
+        <h3> {category} </h3>
+        <div className="card-grid">
+            {
+                images.map( (image, key) => {
+                    return <GifItem key={key} {...image}></GifItem>
+                })
+            }
+        </div>
         </>
     )
-
 }
-
 
---
+++ my-app/src/GifItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const GifItem = ({ title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+export default GifItem;
