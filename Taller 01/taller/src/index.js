4b1834ba946855a8a4b2b89922fff6cc1fddc4ce
JuanPabloParra <juan_pab.parra@uao.edu.co>
2023-02-20 16:02:06 -0500

    Punto 1 del taller

--- Taller 01/taller/src/index.js
+++ Taller 01/taller/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+ 
+import App from './tiempo.js';
 import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
