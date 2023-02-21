10bfaedf9f63f21a92443e9a39a0dfe2dc829573
JuanPabloParra <juan_pab.parra@uao.edu.co>
2023-02-21 18:40:01 -0500

    comentarios en impares

--- Taller 01/taller/src/NumerosImpares.js
+++ Taller 01/taller/src/NumerosImpares.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
 const NumerosImpares = () => {
     const [numero, setNumero] = useState(0);
+// Definimos el estado 'impares' con un valor inicial vacío, que representa los números impares encontrados
+
     const [impares, setImpares] = useState([]);
   
     const onNumeroChange = (event) => {
       const nuevoNumero = parseInt(event.target.value);
       setNumero(nuevoNumero);
     };
-  
+  // Definimos la función 'imprimirImpares' que se ejecuta cuando el usuario hace clic en el botón
+
     const imprimirImpares = () => {
+// Creamos un array vacío para almacenar los números impares encontrados
+
       const imparesEncontrados = [];
+// Usamos un bucle for para recorrer los números impares menores que el número ingresado por el usuario
+
       for (let i = 1; i < numero; i += 2) {
+// Agregamos el número impar encontrado al array 'imparesEncontrados'
+
         imparesEncontrados.push(i);
       }
       setImpares(imparesEncontrados);
