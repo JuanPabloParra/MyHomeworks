1fefb9bd76eb86bdf34375829b1b748c086e217c
JuanPabloParra <juan_pab.parra@uao.edu.co>
2023-03-14 19:57:11 -0500

    TodoList

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,13 +4,14 @@ import App from './App';
 import { GifExpertApp } from './GiftExpertApp';
 import MyComponent from './MyComponent';
 import QuoteComponent from './QuoteComponent';
-
+import TodoList from './TodoList';
 ReactDOM.render(
   <React.StrictMode>
     <GifExpertApp />
     <App />
     <MyComponent />
     <QuoteComponent />
+    <TodoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
