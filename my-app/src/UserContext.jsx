d2b89dbc026b27682dcb4226049150f062412415
Juan Pavón <98887772+JacobS1970@users.noreply.github.com>
2023-03-30 12:43:29 -0500

    modificaciones

--- my-app/src/UserContext.jsx
+++ my-app/src/UserContext.jsx
@@ -1,4 +1,32 @@
-import { createContext } from "react";
+import React, {createContext, useContext, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
+export const UserContext = createContext({ user: null, setUser: () => {} });
 
-export const UserContext = createContext();
+export const LoginPage = () => {
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const onLogin = () => {
+    setUser({
+      id: 313,
+      name: 'Jonathan Lopez',
+      email: 'jlopez0313@hotmail.com'
+    });
+
+    const lastPath = localStorage.getItem('lastPath') || '/';
+    navigate(lastPath, {
+      replace: true
+    });
+  };
+
+  return (
+    <>
+      <div>Login Page</div>
+      <pre>{JSON.stringify(user, null, 3)}</pre>
+      <button className='btn btn-primary' onClick={() => onLogin()}>
+        Login
+      </button>
+    </>
+  );
+};
