417d6d94b42b158e4f597c9c084293d09ed4252c
JuanPabloParra <juan_pab.parra@uao.edu.co>
2023-03-31 01:32:29 -0500

    CHALLENGE 13

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store  from './store';
 import App from './App';
 import { GifExpertApp } from './GiftExpertApp';
 import MyComponent from './MyComponent';
 import QuoteComponent from './QuoteComponent';
 import TodoApp from './TodoApp';
 import { BrowserRouter } from 'react-router-dom';
-import {MainApp} from './MainApp';
+import { MainApp } from './MainApp';
 import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
     <GifExpertApp />
     <MyComponent />
     <QuoteComponent />
